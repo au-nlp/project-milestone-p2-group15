f559f6e0f12fffba25aa060778c7ab3873d18b96
Thomas-Lyhne <201804992@post.au.dk>
2025-10-24 09:25:13 +0200

    fetched all problem names

---
+++ math_arena_datasets.py
@@ -0,0 +1,43 @@
+
+
+
+problem_datasets_2025 = [
+    "MathArena/apex_2025",
+    "MathArena/aime_2025",
+    "MathArena/aime_2025_I",
+    "MathArena/aime_2025_II",
+    "MathArena/hmmt_feb_2025",
+    "MathArena/cmimc_2025",
+    "MathArena/usamo_2025",
+    "MathArena/imo_2025",
+    "MathArena/brumo_2025",
+    "MathArena/imc_2025",
+    "MathArena/kangaroo_2025",
+    "MathArena/kangaroo_2025_1_2",
+    "MathArena/kangaroo_2025_3_4",
+    "MathArena/kangaroo_2025_5_6",
+    "MathArena/kangaroo_2025_7_8",
+    "MathArena/kangaroo_2025_9_10",
+    "MathArena/kangaroo_2025_11_12",
+]
+
+output_datasets_2025 = [
+    "MathArena/apex_2025_outputs",
+    "MathArena/aime_2025_outputs",
+    "MathArena/aime_2025_I_outputs",
+    "MathArena/aime_2025_II_outputs",
+    "MathArena/hmmt_feb_2025_outputs",
+    "MathArena/cmimc_2025_outputs",
+    "MathArena/usamo_2025_outputs",
+    "MathArena/imo_2025_outputs",
+    "MathArena/brumo_2025_outputs",
+    "MathArena/imc_2025_outputs",
+    "MathArena/kangaroo_2025_1-2_outputs",
+    "MathArena/kangaroo_2025_3-4_outputs",
+    "MathArena/kangaroo_2025_5-6_outputs",
+    "MathArena/kangaroo_2025_7-8_outputs",
+    "MathArena/kangaroo_2025_9-10_outputs",
+    "MathArena/kangaroo_2025_11-12_outputs",
+]
+
+
--- readme.md
+++ readme.md
@@ -2,3 +2,21 @@
 ```
 conda env create -f environment.yml
 ```
+
+
+
+
+
+
+# TODOs 
+In section, we will layout the differt subtasks we need to perform. 
+## Statistical analysis (W1)
+- [] Load all datatas
+- [] Display percenage of MathArea dataset which does not contain a category column
+- [] Dicide if we need to drop some comptetions if percentage is not too high
+- [] Create radar chart viz of perfornce of eacn catagty + table 
+- [] Think of more things relevant to create the hypthesis - ex large vs low
+
+## Performance hypothesis (W2)
+- 
+
