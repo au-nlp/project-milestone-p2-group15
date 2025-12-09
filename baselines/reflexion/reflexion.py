29717d90eacbb6cb80eec1531289deb8e77a4b29
Priyanka Ananth <killersaurus161@gmail.com>
2025-12-09 10:03:08 +0100

    Deepseek test written

--- baselines/reflexion/reflexion.py
+++ baselines/reflexion/reflexion.py
@@ -1,5 +1,5 @@
 from enum import Enum
-from prompt_template import Reflector, Evaluator
+from models.prompt_template import Reflector, Evaluator
 from models.azure_api import Model
 
 # Evaluator function
